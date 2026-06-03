b85ac07df73fc0e4d1a11693acfbdb8367904dbf
admmsukasha-svg <adm.msukasha@gmail.com>
2026-06-03 14:52:05 +0500

    Add 'My Account' link to header actions

--- main.js
+++ main.js
@@ -40,6 +40,10 @@ const HEADER_HTML = `
       <button type="submit"><i class="fas fa-search"></i> Search</button>
     </form>
     <div class="header-actions">
+      <a href="my.html" class="hdr-btn">
+        <i class="fas fa-user"></i>
+        <span>My Account</span>
+      </a>
       <a href="wishlist.html" class="hdr-btn">
         <i class="far fa-heart"></i>
         <span>Wishlist</span>
